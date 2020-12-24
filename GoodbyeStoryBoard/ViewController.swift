7f66869d1426adf15e47bd9c39477dda83de6f46
codegangsk <kjhj0604@gmail.com>
2020-12-24 15:41:11 +0900

    add gitignore

---
+++ GoodbyeStoryBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoodbyeStoryBoard
+//
+//  Created by Sophie Kim on 2020/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
